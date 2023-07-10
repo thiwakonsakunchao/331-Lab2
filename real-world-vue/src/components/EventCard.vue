d0d9d8ff70faf9f0a847ed9186073d904a0eef08
Thiwakon Sakunchao <thiwakon_sakunchao@cmu.ac.th>
2023-07-10 23:18:12 +0700

    4.3

--- real-world-vue/src/components/EventCard.vue
+++ real-world-vue/src/components/EventCard.vue
@@ -5,6 +5,18 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
+const event = ref({
+    id: 5928101,
+    category: 'animal welfare',
+    title: 'Cat Adoption Day',
+    description: 'Find your new feline friend at this event.',
+    location: 'Meow Town',
+    date: 'January 28, 2022',
+    time: '12:00',
+    petsAllowed: true,
+    organizer: 'Kat Laydee'
+})
 </script>
 
 <style scoped>
