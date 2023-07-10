83be25b723703731881fc8c534f82169d9d84b72
Thiwakon Sakunchao <thiwakon_sakunchao@cmu.ac.th>
2023-07-10 23:19:26 +0700

    4.4

--- real-world-vue/src/components/EventCard.vue
+++ real-world-vue/src/components/EventCard.vue
@@ -1,6 +1,10 @@
 
 <template>
     <div class="event-class">      
+        <div class="event-card">
+            <span>@{{ event.time }} on {{ event.date }}</span>
+            <h4>{{ event.title }}</h4>
+        </div>
     </div>
 </template>
 
