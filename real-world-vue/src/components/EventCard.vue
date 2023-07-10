44618a51c8003b33296c7f9719dfd7e564a501fc
Thiwakon Sakunchao <thiwakon_sakunchao@cmu.ac.th>
2023-07-10 23:14:21 +0700

    4.2

--- real-world-vue/src/components/EventCard.vue
+++ real-world-vue/src/components/EventCard.vue
@@ -1,10 +1,23 @@
 
 <template>
-    <div class="hello">      
+    <div class="event-class">      
     </div>
 </template>
 
 <script setup lang="ts">
 </script>
 
-<style scoped></style>
+<style scoped>
+.event-card{
+    padding: 20px;
+    width: 250px;
+    cursor: pointer;
+    border: 1px solid #39495c;
+    margin-bottom: 18px;
+}
+
+.event-card:hover {
+    transform: scale(1.01);
+    box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
