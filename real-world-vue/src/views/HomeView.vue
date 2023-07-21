e6767d26899d738eb9afd25bc7c217dda384a159
Thiwakon Sakunchao <thiwakon_sakunchao@cmu.ac.th>
2023-07-21 10:12:32 +0700

    4.7

--- real-world-vue/src/views/HomeView.vue
+++ real-world-vue/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import EventCard from '../components/EventCard.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <EventCard />
   </main>
 </template>
